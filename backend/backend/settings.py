e58f327fbb41620c7614ff5763ac751fae63718a
greedharvivek_45 <greedharvivek@gmail.com>
2026-04-17 10:23:04 +0530

    chnew

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CORS_ALLOW_ALL_ORIGINS = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -70,7 +70,7 @@ TEMPLATES = [
         },
     },
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
